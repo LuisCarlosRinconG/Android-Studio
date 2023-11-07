bf358a64ab787d6bac66409304384516b3025f5c
Luis Carlos Rincón <125197117+LuisCarlosRinconG@users.noreply.github.com>
2023-11-07 11:02:29 -0600

    Arreglo de registro sqlite

--- app/src/main/java/com/example/crono_app/Usuarios.java
+++ app/src/main/java/com/example/crono_app/Usuarios.java
@@ -1,5 +1,6 @@
 package com.example.crono_app;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -27,13 +28,17 @@ public class Usuarios extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1){
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + tablaUser);
-        sqLiteDatabase.execSQL(tablaUser);
     }
 
-    public void agregarUsuarios(String documento, String nombre, String correo, String contrasena){
-        SQLiteDatabase bd=getWritableDatabase();
-        if(bd!=null){
-            bd.execSQL("INSERT INTO Usuarios VALUES ('"+documento+"','"+nombre+"','"+correo+"','"+contrasena+"')");
+    public void agregarUsuarios(String documento, String nombre, String correo, String contrasena) {
+        SQLiteDatabase bd = getWritableDatabase();
+        if (bd != null) {
+            ContentValues values = new ContentValues();
+            values.put("DOCUMENTO", documento);
+            values.put("NOMBRE", nombre);
+            values.put("CORREO", correo);
+            values.put("CONTRASENA", contrasena);
+            bd.insert("Usuarios", null, values);
             bd.close();
         }
     }
