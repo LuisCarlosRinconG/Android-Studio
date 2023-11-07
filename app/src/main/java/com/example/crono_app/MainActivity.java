2bd05c0f98a1766a6d894a394986bc794025b2e0
Luis Carlos Rincón <125197117+LuisCarlosRinconG@users.noreply.github.com>
2023-11-07 01:06:49 -0600

    Paso de paginas

--- app/src/main/java/com/example/crono_app/MainActivity.java
+++ app/src/main/java/com/example/crono_app/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.crono_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //Cambio de de pantalla de inicio a registro
+    //Creamos una variable que almacenene el voton registro(check in) y otra que almacene login(get in)
+    Button registro;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Guardamos el valor del boteon en la variable creada
+        registro=findViewById(R.id.register);
+        login=findViewById(R.id.inicio);
+
+        // Activity Registro
+        registro.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, Registro.class);
+                startActivity(intent);
+            }
+        });
+
+        //Activity Login
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
