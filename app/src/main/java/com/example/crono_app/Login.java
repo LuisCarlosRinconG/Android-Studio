0c6a89df5f45cc4608d4f8ac3c4812d489226b43
Luis Carlos Rincón <125197117+LuisCarlosRinconG@users.noreply.github.com>
2023-11-07 23:53:46 -0600

    conf

--- app/src/main/java/com/example/crono_app/Login.java
+++ app/src/main/java/com/example/crono_app/Login.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+    EditText Password,Email;
+    Button Entrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Email = (EditText) findViewById(R.id.email);
+        Password = (EditText) findViewById(R.id.password);
+        Entrar = (Button) findViewById(R.id.entrar);
+
     }
 }
