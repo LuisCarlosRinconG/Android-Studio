948dd73b4ea4c0bcac36f0d0cbd0a45cb4b89717
Luis Carlos Rincón <125197117+LuisCarlosRinconG@users.noreply.github.com>
2023-11-08 00:26:02 -0600

    dos

--- app/src/main/java/com/example/crono_app/Login.java
+++ app/src/main/java/com/example/crono_app/Login.java
@@ -1,25 +1,58 @@
 package com.example.crono_app;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.crono_app.Inicio;
 
 public class Login extends AppCompatActivity {
-    EditText Password,Email;
+    EditText Password, Email;
     Button Entrar;
+
+    // Agrega una referencia a tu base de datos SQLite
+    SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Email = (EditText) findViewById(R.id.email);
-        Password = (EditText) findViewById(R.id.password);
-        Entrar = (Button) findViewById(R.id.entrar);
 
+        Email = findViewById(R.id.email);
+        Password = findViewById(R.id.password);
+        Entrar = findViewById(R.id.entrar);
+
+        // Inicializa tu base de datos aquí, por ejemplo:
+        database = openOrCreateDatabase("Usuarios", MODE_PRIVATE, null);
+
+        Entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = Email.getText().toString();
+                String password = Password.getText().toString();
+
+                // Realiza la consulta en la base de datos
+                Cursor cursor = database.rawQuery("SELECT * FROM tabla_usuarios WHERE email = ? AND password = ?",
+                        new String[]{email, password});
+
+                if (cursor.moveToFirst()) {
+                    // Los datos del usuario existen en la base de datos, navega a la actividad "Inicio"
+                    Intent intent = new Intent(Login.this, Inicio.class);
+                    startActivity(intent);
+                } else {
+                    // Los datos no coinciden o no existen en la base de datos
+                    Toast.makeText(Login.this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+                }
+
+                cursor.close();
+            }
+        });
     }
 }
